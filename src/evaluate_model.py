dd0341b7cc42002feaea55cb33c945978eed6cf9
DeepBlockDeepak <jordan.medina1729@gmail.commit>
2024-01-07 23:05:29 -0700

    Added SVM model for accuracy comparison. It's not great. Might not keep this around in final project.

--- src/evaluate_model.py
+++ src/evaluate_model.py
@@ -3,5 +3,6 @@ from sklearn.metrics import accuracy_score
 def evaluate_model(model, X_val, y_val):
     predictions = model.predict(X_val)
     accuracy = accuracy_score(y_val, predictions)
-    print(f"Accuracy: {accuracy:.2f}")
+    model_name = type(model).__name__
+    print(f"Model: {model_name}, Accuracy: {accuracy:.2f}")
     return predictions
---
+++ src/svm_train.py
@@ -0,0 +1,26 @@
+from sklearn.svm import SVC
+import joblib
+from src.evaluate_model import evaluate_model
+from sklearn.model_selection import train_test_split, GridSearchCV
+
+def train_svm_model(X_train, y_train):
+    
+    svm_model = SVC(kernel='rbf', gamma='scale', C=1, probability=True)
+    svm_model.fit(X_train, y_train)
+
+    # saving the svm model
+    joblib.dump(svm_model, 'models/titanic_svm_model.pkl')
+    
+    return svm_model
+
+def svm_main(X, y):
+    # Splitting the data
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=0)
+
+    # Train the SVM model
+    svm_model = train_svm_model(X_train, y_train)
+
+    # Evaluate the model
+    svm_predictions = evaluate_model(svm_model, X_val, y_val)
+
+    return svm_predictions
